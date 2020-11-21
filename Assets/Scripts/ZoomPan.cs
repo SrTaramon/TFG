6397dd0f9ecb9c57605d82e7d8e41e9044067189
SrTaramon <sergitaramon21@gmail.com>
2020-11-21 20:49:42 +0100

    Zoom&Pan
    Zoom and Pan mechnaics done, also first mapa look

---
+++ Assets/Scripts/ZoomPan.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomPan : MonoBehaviour
+{
+    Vector3 touchStart;
+    public float zoomOutMin = 5;
+    public float zoomOutMax = 8;
+    // Start is called before the first frame update
+    void Start()
+    {
+         Camera.main.transform.position = new Vector3(0, 0, -10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.touchCount == 2){
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+
+            Vector2 touch0PrevPos = touch0.position - touch0.deltaPosition;
+            Vector2 touch1PrevPos = touch1.position - touch1.deltaPosition;
+
+            float prevMagnitude = (touch0PrevPos - touch1PrevPos).magnitude;
+            float currentMagnitude = (touch0.position - touch1.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            Zoom(difference * 0.01f);
+
+        } else if (Input.GetMouseButton(0)){
+            Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Camera.main.transform.position += direction;
+        }
+
+        Zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    void Zoom(float increment){
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+}
