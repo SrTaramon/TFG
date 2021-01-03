97c34a0e3dd538a99b8b9025f9bea0bfa8aaed61
SrTaramon <sergitaramon21@gmail.com>
2021-01-03 19:50:26 +0100

    Scripts added
    Scripts added

---
+++ Assets/Scripts/Menu-Map/CamiAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamiAnimation : MonoBehaviour
+{
+
+    public GameObject[] cami;
+
+    private float timer = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cami[0].gameObject.GetComponent<Animation>().Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        switch (timer){
+            case 40:
+                cami[1].gameObject.GetComponent<Animation>().Play();
+                Debug.Log("1");
+            break;
+            case 20:
+                cami[2].gameObject.GetComponent<Animation>().Play();
+                Debug.Log("2");
+            break;
+            case 0:
+                cami[3].gameObject.GetComponent<Animation>().Play();
+                Debug.Log("3");
+            break;
+        }
+    }
+}
