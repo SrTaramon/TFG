83642ea6812b70b444cbbc1ba029233e91693004
SrTaramon <sergitaramon21@gmail.com>
2021-04-18 13:57:29 +0200

    Insigneas done

--- Assets/Scripts/Extres/AvatarPick.cs
+++ Assets/Scripts/Extres/AvatarPick.cs
@@ -7,6 +7,19 @@ using UnityEngine.SceneManagement;
 
 public class AvatarPick : MonoBehaviour
 {
+
+    public GameObject panelIntro, panelAvatar;
+
+    void Start(){
+        panelIntro.SetActive(true);
+        panelAvatar.SetActive(false);
+    }
+
+    public void activateAvatarSelection(){
+        panelIntro.SetActive(false);
+        panelAvatar.SetActive(true);
+    }
+
     public void pickAvatar(Button choice){
         SaveData.current.selectedAvatar = Int32.Parse(choice.gameObject.name);
         SaveData.current.introDone = true;
--- Assets/Scripts/Extres/Insigneas.cs
+++ Assets/Scripts/Extres/Insigneas.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 using UnityEngine.SceneManagement;
 
 public class Insigneas : MonoBehaviour
 {
 
-    public GameObject i1, i2, i3, i4;
+    public GameObject i1, i2, i3, i4, e1, e2, e3, panelIns;
+
+    public Text crono, titol;
+
+    private string t1 = "Cartas reproductoras";
+    private string t2 = "Construcción de símbolos";
+    private string t3 = "Pelotas emocionales";
+    private string t4 = "Destruyendo mitos";
+    private string t5 = "Fuga de letras";
+    private string t6 = "Memory";
+
 
     // Start is called before the first frame update
     void Start()
@@ -49,8 +61,68 @@ public class Insigneas : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    public void infoInsignea(Button button){
+        int id = Int32.Parse(button.gameObject.name);
+        switch (id){
+            case 1:
+                titol.text = t1;
+                crono.text = transformTime(SaveData.current.temps1);
+                activateStarts(SaveData.current.estrelles1);
+                break;
+            case 2:
+                titol.text = t2;
+                crono.text = transformTime(SaveData.current.temps2);
+                activateStarts(SaveData.current.estrelles2);
+                break;
+            case 3:
+                titol.text = t3;
+                crono.text = transformTime(SaveData.current.temps3);
+                activateStarts(SaveData.current.estrelles3);
+                break;
+            case 4:
+                titol.text = t4;
+                crono.text = transformTime(SaveData.current.temps4);
+                activateStarts(SaveData.current.estrelles4);
+                break;
+            default:
+                break;
+        }
+        panelIns.SetActive(true);
+    }
+
+    private string transformTime(int timeAc){
+        if (timeAc >= 60){
+            int min = timeAc % 60;
+            int sec = timeAc - (60 * min);
+            return min.ToString() + ":" + sec.ToString() + "min";
+        } else {
+            return timeAc.ToString() + "s";
+        }
+    }
+
+    private void activateStarts(int stars){
+        switch (stars){
+            case 1:
+                e1.SetActive(true);
+                break;
+            case 2:
+                e1.SetActive(true);
+                e2.SetActive(true);
+                break;
+            case 3:
+                e1.SetActive(true);
+                e2.SetActive(true);
+                e3.SetActive(true);
+                break;
+        }
+    }
+
+    public void stopInfo(){
+        titol.text = "";
+        crono.text = "";
+        e1.SetActive(false);
+        e2.SetActive(false);
+        e3.SetActive(false);
+        panelIns.SetActive(false);
     }
 }
